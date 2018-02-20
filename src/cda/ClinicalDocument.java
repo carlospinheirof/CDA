b199029826979b86846c50d123b9a3fc702ce347
carlospinheirof <carlos.pinheiro.f@gmail.com>
2018-02-20 13:25:43 -0300

    Custodian feito.

--- src/cda/ClinicalDocument.java
+++ src/cda/ClinicalDocument.java
@@ -9,11 +9,13 @@ public class ClinicalDocument {
 	
 	//-<ClinicalDocument classCode="DOCCLIN" xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns="urn:hl7-org:v3">
 	RecordTarget RecordTarget = new RecordTarget();
+	Custodian Custodian = new Custodian();
 	
 	public String CreateCda(Paciente paciente){
 		cda = cda + "\n";
 		cda = cda + RecordTarget.createRecortTarget(paciente);
-		
+		//author
+		cda = cda + Custodian.createCustodian(paciente);
 		cda = cda + "</ClinicalDocument>";
 		return cda;
 	}
--- src/cda/Custodian.java
+++ src/cda/Custodian.java
@@ -1,5 +1,20 @@
 package cda;
 
-public class Custodian {
+import pessoas.Paciente;
 
+public class Custodian {
+		
+	private String custodian;
+	
+	public String createCustodian (Paciente paciente){
+		custodian = "<custodian>" + "\n";
+		custodian = custodian + "<assignedCustodian>" + "\n" + 
+					"<representedCustodianOrganization>" + "\n";
+		custodian = custodian + "<id extension=" + "\"MPred\"" +" root=" +
+					"\"2.16.840.1.113883.3.933\"" + "/>" + "\n";
+		custodian = custodian + "<name>MyPred</name>" + "\n";
+		custodian = custodian + "</representedCustodianOrganization>" + "\n" + "</assignedCustodian>" 
+		 + "\n";
+		return custodian + "</custodian>" + "\n";
+	}
 }
